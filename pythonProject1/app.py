a41732b27419f7110779b7c0911f4548d2bfe4fa
AngelSigueros <pickassblog@gmail.com>
2023-12-12 16:02:35 +0100

    Python primeros ejemplos

---
+++ pythonProject1/app.py
@@ -0,0 +1,36 @@
+print('PyThon!')
+# print('PyThon!')
+
+"""
+comentario
+"""
+
+# dsfsdf
+# dsfgdsfds
+# dsgfdsg
+# fdgdfgsg
+
+# strip() elimina los espacios en blanco al principio y al final de una cadena
+s = "   Hola Mundo!   "
+print(s.strip())  # Salida: "Hola Mundo!"
+
+# lower() convierte todos los caracteres de una cadena a minúsculas
+s = "HOLA MUNDO!"
+print(s.lower())  # Salida: "hola mundo!"
+
+# upper() convierte todos los caracteres de una cadena a mayúsculas
+s = "hola mundo!"
+print(s.upper())  # Salida: "HOLA MUNDO!"
+
+# replace() reemplaza una subcadena en una cadena por otra subcadena
+s = "Hola mundo!"
+print(s.replace("mundo", "Python"))  # Salida: "Hola Python!"
+
+# split() divide una cadena en una lista donde cada palabra es un elemento de la lista
+s = "Hola mundo!"
+print(s.split())  # Salida: ['Hola', 'mundo!']
+
+# format() formatea una cadena utilizando variables
+nombre = "Python"
+mensaje = "Hola, {}!"
+print(mensaje.format(nombre))  # Salida: "Hola, Python!"
