521d610f5d0e31b7e35753e6c887a67082b3d192
AngelSigueros <pickassblog@gmail.com>
2023-12-15 11:17:33 +0100

    Excepciones py

---
+++ 6-excepciones/1-try-except.py
@@ -0,0 +1,24 @@
+# try - except permite capturar errores y que el programa no se detenga
+
+try:
+    print(hola)
+except:
+    print("Error")
+
+print("Esto está bien")
+
+# try except - múltiple, con varios except para capturar diferentes tipos de error
+try:
+    num1 = int(input("Introduce un numero:"))
+    num2 = int(input("Introduce otro numero:"))
+    lista = [1, 2, 3, 4]
+    print(lista[676])
+    result = num1 / num2
+except ValueError:
+    print("ValueError1: formato de número incorrecto.")
+except ZeroDivisionError:
+    print("ZeroDivisionError: error en división.")
+except IndexError:
+    print("IndexError: acceso fuera del límite de la lista")
+
+print("fin")
---
+++ 6-excepciones/3-raise.py
@@ -0,0 +1,20 @@
+
+# raise sirve para lanzar una excepción, esto permite centralizar la gestión de excepciones en un mismo punto
+# de un programa, haciendo que las funciones que se invocan desde ese punto lancen excepciones en vez de gestionarlas
+
+emails = ["dani@example.com", "alan@example.com", "aroa@example.com"]
+
+
+def validate_email(email):
+    if email in emails:
+        raise Exception("Email ya registrado")
+    else:
+        print("Email válido")
+
+
+try:
+    validate_email("alan@example.com")
+except Exception as e:
+    print("Error capturado")
+
+print("fin")
